f7835bda839b207ed4b53d5b064979670e559b35
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-03-29 23:29:59 +0800

    Update subject1.cpp

--- subject1.cpp
+++ subject1.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             int tmp=target-nums[i];
-            if(record.find(tmp)!=record.end())
+            if(record.find(tmp)!=record.end())      //find算法返回目标的迭代器，如果没找到返回一个end()
             {
                 result.push_back(record[tmp]);
                 result.push_back(i);
