5d5ae72ca44b8ab432c4d57b63d989b9d37acd1e
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-04-01 09:27:50 +0800

    Update subject6.cpp

--- subject6.cpp
+++ subject6.cpp
@@ -15,10 +15,9 @@ class Solution {
 public:
     bool isPalindrome(int x) {
     if(x<0||x%10==0) return false;
-    if(x>0&&x%10==0) return false;
     long y=0;
     int temp=x;
-    while(temp!=0){
+    while(temp){
         y=y*10+temp%10;
         temp/=10;
     }
