7fd196f65c359bb2b6c6dd6a0b618152f72ed042
KoraCode <62596768+KoraCode@users.noreply.github.com>
2020-07-16 16:23:58 +0800

    Update subject4.cpp

--- subject4.cpp
+++ subject4.cpp
@@ -15,3 +15,19 @@ public:
         return max(maxlen, right-left-1);   //因为求的是子串因此要减1
     }
 };
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> m(128,0);       //创建一个vector用于记录相关数据的最后的位置
+        int i=0;
+        int ans=0;
+        for(int j=0;j<s.size();j++)
+        {
+            i=max(i,m[s[j]]);           //每次比较当前指针的数据是否重复出现，如果重复就改变当前的指针位置
+            m[s[j]]=j+1;                //保存当前数据的下一个位置
+            ans=max(ans,j-i+1);         
+        }
+        return ans;
+    }
+};
